7418500e1ec038038a3086c5237f65240a5368ad
Jameel <jameel.sameemi@gmail.com>
2021-11-10 17:32:52 -0700

    added comments

--- conditionalSum.js
+++ conditionalSum.js
@@ -1,3 +1,10 @@
+
+/* Create a function named conditionalSum that will be given
+ an array of numbers and a condition, in this case odd or 
+ even. Given this condition, add up only the values which 
+ match that condition. If no values match the condition,
+ return 0.
+ */
 const conditionalSum = function(values, condition) {
 let isEven=0;
 for (i=0; i<values.length; i++) {
